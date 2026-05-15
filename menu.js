4bc280d11663601c3e81ed9bc25ad7271687ad0e
Kmirka <schookg7@gmail.com>
2026-05-15 17:56:05 +0300

    save 15.05

--- menu.js
+++ menu.js
@@ -1,51 +1,30 @@
-// ===============================
-// SportShop JavaScript
-// ===============================
-
-// Знаходимо всі кнопки "Купити"
-const buyButtons = document.querySelectorAll(".buy-btn");
 const items = [
     {
         name: "потжнокросівки",
         img: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcRF3bcn8mmGwintvYTt6TfDl0xd549TVdkZ9GVERBjt4S7ItdE2I8UtjnXNHwA1Jly940sPIU7XA-bHiwIsGcBVLwyf5en28piQQt1mcHThMBAddzyQkiysVjoV5Os2tR5y4Rfnc8ZZZIE&usqp=CAc",
         price: "67 долларів"
-    }
-]
-// Лічильник товарів у кошику
-let cartCount = 0;
+    },
+    {
+        name: "просто кросівки",
+        img: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcRF3bcn8mmGwintvYTt6TfDl0xd549TVdkZ9GVERBjt4S7ItdE2I8UtjnXNHwA1Jly940sPIU7XA-bHiwIsGcBVLwyf5en28piQQt1mcHThMBAddzyQkiysVjoV5Os2tR5y4Rfnc8ZZZIE&usqp=CAc",
+        price: "90 долларів"
+    },
+];
 
-// Перебираємо всі кнопки
-buyButtons.forEach((button) => {
+const shop = document.getElementById("shop");
 
-    // Додаємо подію кліку
-    button.addEventListener("click", () => {
+items.forEach(item => {
+    const card = document.createElement("div");
+    card.classList.add("card");
 
-        // Знаходимо карточку товару
-        const card = button.parentElement;
+    card.innerHTML = `
+        <img src="${item.img}" alt="${item.name}">
+        <div class="card-body">
+            <h2>${item.name}</h2>
+            <div class="price">${item.price}</div>
+            <button class="buy-btn">Купити</button>
+        </div>
+    `;
 
-        // Назва товару
-        const productName =
-            card.querySelector("h3").innerText;
-
-        // Ціна товару
-        const productPrice =
-            card.querySelector("p").innerText;
-
-        // Збільшуємо кількість товарів
-        cartCount++;
-
-        // Повідомлення
-        alert(
-            "🛒 Товар додано в кошик!\n\n" +
-            "Товар: " + productName + "\n" +
-            "Ціна: " + productPrice + "\n\n" +
-            "Товарів у кошику: " + cartCount
-        );
-
-        // Зміна кнопки після натискання
-        button.innerText = "✓ Додано";
-        button.style.backgroundColor = "green";
-
-    });
-
+    shop.appendChild(card);
 });
